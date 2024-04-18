43e871e0dbad07b30c1752b39fbfd0e3c7222fb1
Colton1skees <Colton1skees@gmail.com>
2024-04-18 13:34:54 -0400

    Example triton integration

--- WinDNA/src/dllmain.cpp
+++ WinDNA/src/dllmain.cpp
@@ -12,21 +12,11 @@ csh handle;
 int globalCount = 0;
 void __fastcall InstrCallbackCallRets(Internals::VirtualCpu* vCpu, u64 flowDstAddr)
 {
-    
-    return;
+   return;
    u64 rip;
    vCpu->GetRegister(Internals::RegisterId::RIP, &rip, 8);
 
-
-   //u64 rip = vCpu->GetPC();
-  // rip = flowDstAddr;
-
-   //printf("call or ret destination = %#018llx\n", rip);
-
-   uint8_t disassemblyBuffer[32];
-
    unsigned int pageSize = 0x1000 - ((((DWORD)rip)) & 0xFFF);
- //  std::cout << "reading page size: " << pageSize << std::endl;
    if (pageSize > 15)
        pageSize = 15;
 
@@ -36,29 +26,17 @@ void __fastcall InstrCallbackCallRets(Internals::VirtualCpu* vCpu, u64 flowDstAd
    if (isReadingMem)
        tempCpu = 0;
 
-   bool hasRead = Internals::VirtualCpuHelper::ReadVirtualCpuMemory(vCpu, rip & (u64)0xFFFFFFFFFFFF, &disassemblyBuffer, pageSize);
- //  std::cout << "hasRead: " << hasRead << std::endl;
    if (tempCpu != nullptr)
        tempCpu->bIsReadingMem = false;
 
    cs_insn* insn;
+   uint8_t disassemblyBuffer[32];
    auto count = cs_disasm(handle, disassemblyBuffer, 15, rip, 1, &insn);
-   globalCount++;
-
-   if (globalCount % 10000 == 0)
-   {
-       std::cout << "disassembled count: " << globalCount << std::endl;
-       printf("0x%" PRIx64 ":\t%s\t\t%s\n", insn[0].address, insn[0].mnemonic,
-               insn[0].op_str);
-   }
- //  printf("0x%" PRIx64 ":\t%s\t\t%s\n", insn[0].address, insn[0].mnemonic,
-   //    insn[0].op_str);
-   
 }
 
 void __fastcall InstrCallbackPreWriteMemory(Internals::VirtualCpu* vCpu, u64 address, u64 dataSize)
 {
-  //  std::cout << "pre-write memory rip: 0x" << vCpu->GetPC() << std::endl;
+
 }
 
 void __fastcall InstrCallbackPostWriteMemory(Internals::VirtualCpu* vCpu, u64 address, void* pBuffer, u64 dataSize)
@@ -89,7 +67,6 @@ fDispatcherLoop dispatcherLoop;
 
 void* __fastcall h_DispatcherLoop(Internals::VirtualCpu* vCpu, u64 a2)
 {
-   // std::cout << "dispatcher loop at rip: 0x" << std::hex << vCpu->GetPC() << std::endl;
     Tainter::HandleTaintInstruction(vCpu);
     return dispatcherLoop(vCpu, a2);
 }
@@ -110,16 +87,6 @@ void Initialize()
     DetourUpdateThread(GetCurrentThread());
     DetourAttach(&(PVOID&)dispatcherLoop, h_DispatcherLoop);
     DetourTransactionCommit();
-
-
-    /*
-    // Initialize capstone
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
-    {
-        printf("failed to load capstone.");
-        return;
-    }
-    */
 
     // Execute boilerplate code for registering callbacks.
     RegisterInstrumentationCallbacks();
